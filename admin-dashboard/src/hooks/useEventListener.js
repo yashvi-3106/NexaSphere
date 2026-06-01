dc3bfde366a5bab1aaef788b8ba165a373e6d299
Pratikshya <2406043@kiit.ac.in>
2026-06-01 18:33:34 +0530

    fix: Validate callback parameters in useEventListener
    Signed-off-by: Pratikshya <2406043@kiit.ac.in>

--- admin-dashboard/src/hooks/useEventListener.js
+++ admin-dashboard/src/hooks/useEventListener.js
@@ -3,7 +3,14 @@ import { eventEmitter } from '../services/eventEmitter';
 
 export function useEventListener(event, handler) {
   useEffect(() => {
+    if (!eventEmitter || typeof eventEmitter.on !== 'function') return;
+    if (typeof handler !== 'function') return;
+
     eventEmitter.on(event, handler);
-    return () => eventEmitter.off(event, handler);
+    return () => {
+      if (eventEmitter && typeof eventEmitter.off === 'function') {
+        eventEmitter.off(event, handler);
+      }
+    };
   }, [event, handler]);
 }
